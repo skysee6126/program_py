dc6516dd4f8acb6ee5e809695f12d8288c29b7b6
skysee6126 <chooshinhye@gmail.com>
2021-04-29 18:11:27 +0900

    commit

---
+++ gui_basic.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("GUI program")
+root.geometry("640x480")
+#root.geometry("640x480+300+100") #가로, 세로, x좌표, y좌표
+
+root.resizable(False,False)
+root.mainloop()
