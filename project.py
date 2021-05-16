4dcda4a919c878a6d6138b6b03a0686e598da577
skysee6126 <chooshinhye@gmail.com>
2021-05-16 20:39:11 +0900

    add/delete file

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import tkinter.ttk as ttk
 from tkinter import *
+from tkinter import filedialog
 
 root = Tk()
 root.title("GUI program")
@@ -7,21 +8,34 @@ root.title("GUI program")
 file_frame = Frame(root)
 file_frame.pack(fill="x", padx=5, pady=5)
 
-btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text="Add file")
+
+def add_file():
+  files = filedialog.askopenfilenames(title="choose image files", \
+    filetypes=(("PNG file", "*.png"), ("All files", "*.*")), \
+    initialdir="C:/")
+  for file in files:
+    list_file.insert(END, file)
+
+def del_file():
+  for index in reversed(list_file.curselection()):
+    list_file.delete(index)
+
+
+btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text="Add file", command=add_file)
 btn_add_file.pack(side="left")
-btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text="Delete select")
+btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text="Delete select", command=del_file)
 btn_add_file.pack(side="right")
 
 #list frame
 list_frame= Frame(root)
-list_frame.pack(padx=5, pady=5)
+list_frame.pack(fill="both", padx=5, pady=5)
  
 scrollbar = Scrollbar(list_frame)
 scrollbar.pack(side="right", fill="y")
 
-list_frame = Listbox(list_frame, selectmode="extended", height=15, yscrollcommand=scrollbar.set)
-list_frame.pack(side="left", fill="both", expand=True, padx=5, pady=5)
-scrollbar.config(command=list_frame.yview)
+list_file = Listbox(list_frame, selectmode="extended", height=15, yscrollcommand=scrollbar.set)
+list_file.pack(side="left", fill="both", expand=True, padx=5, pady=5)
+scrollbar.config(command=list_file.yview)
 
 
 path_frame = LabelFrame(root, text="Save path")
